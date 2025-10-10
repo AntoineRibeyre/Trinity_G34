a2637264f551c60054585e0876f05b7432363b4c
Antoine <antoine.ribeyre@epitech.eu>
2025-10-10 09:43:26 +0200

    build(side-navbar): begin

--- frontend/src/app/shared/components/side-nav/side-nav.ts
+++ frontend/src/app/shared/components/side-nav/side-nav.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-side-nav',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './side-nav.html',
   styleUrl: './side-nav.css'
 })
